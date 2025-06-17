b2387bd11adbdf3fe7eeb46f77e74beeedbc4fdf
Arsen Fedotov <markos.1986@mail.ru>
2025-06-17 11:37:14 +0300

    Изменили строки Main и doSomething(1234)

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -6,12 +6,18 @@ public class MainApplication {
         System.out.println("Hello World!");
         System.out.println("1001");
         System.out.println("---");
+        System.out.println("---");
+        System.out.println("---");
+        System.out.println("---");
 
             }
 
     public static void doSomething() {
         System.out.println(1);
         System.out.println(20);
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
         System.out.println(10);
 
     }
